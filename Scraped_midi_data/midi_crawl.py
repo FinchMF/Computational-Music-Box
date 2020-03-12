57c1b47a07a39bcceb7e86bae39cdc33ddd7bfd3
Matt Finch <matt.fiinch92@gmail.com>
2020-03-12 18:34:14 -0400

    200312-1

---
+++ Scraped_midi_data/midi_crawl.py
@@ -0,0 +1,12 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+import wget
+
+class Crawler:
+
+    link = 
+
+    def __init__(self, composer, link):
+        self.composer = composer
+        self.link = link
